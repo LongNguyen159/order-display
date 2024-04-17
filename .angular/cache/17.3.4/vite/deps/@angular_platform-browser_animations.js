ca6b326a221856d6d69fd1fa64647ebe99dec14a
long <nguyenthanhlong@morenergy.net>
2024-04-17 16:00:02 +0200

    refactor: move node modules to frontend folder

---
+++ .angular/cache/17.3.4/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,22 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-FR7SNQSS.js";
+import "./chunk-XSAIAJ25.js";
+import "./chunk-ZSB5FPDL.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-OI6EX5JM.js";
+import "./chunk-SXIXOCJ4.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
