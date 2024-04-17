74024802861e66e126b1ca822fa1ca1ab33b1399
long <nguyenthanhlong@morenergy.net>
2024-04-17 15:53:45 +0200

    feat: new page: homepage component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ViewPageComponent } from './pages/view-page/view-page.component';
 import { ControllerPageComponent } from './pages/controller-page/controller-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
     {
@@ -11,4 +12,13 @@ export const routes: Routes = [
         path: 'controller',
         component: ControllerPageComponent
     },
+    {
+        path: 'home',
+        component: HomePageComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
 ];
