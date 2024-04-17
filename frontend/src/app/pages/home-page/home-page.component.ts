94d71ec4244c117d22123b2ce0668d3af6541662
long <nguyenthanhlong@morenergy.net>
2024-04-17 16:12:17 +0200

    feat: home page

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule, CommonModule, RouterModule
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
