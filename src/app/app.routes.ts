1d8b5d53c6c811aab20a6bfb615281cecf41e5a2
long <nguyenthanhlong@morenergy.net>
2024-04-20 00:35:00 +0200

    refactor file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,13 @@
 import { Routes } from '@angular/router';
 import { ViewPageComponent } from './pages/view-page/view-page.component';
-import { ControllerPageComponent } from './pages/controller-page/controller-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-
 export const routes: Routes = [
     {
         path: 'view',
         component: ViewPageComponent
     },
     {
-        path: 'controller',
-        component: ControllerPageComponent
-    },
-    {
-        path: 'home',
-        component: HomePageComponent
-    },
-    {
         path: '',
-        redirectTo: '/home',
+        redirectTo: '/view',
         pathMatch: 'full'
     }
 ];
